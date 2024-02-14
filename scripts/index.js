8307d363446279707529b186f972235d44ef0657
dantum <dantum150@yandex.ru>
2024-02-14 12:04:23 +0300

    Сдача 5 спринта от 14.02.2024

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const cardsList = document.querySelector('.places__list')
+
+function createCard(cardInfo, removeCardFunction) {
+  const newCardTemplate = document.querySelector('#card-template').cloneNode(true).content
+
+  const newCardElement = newCardTemplate.querySelector('.places__item')
+
+  newCardElement.querySelector('.card__title').textContent = cardInfo.name
+
+  newCardElement.querySelector('.card__image').setAttribute('src', cardInfo.link)
+
+  newCardElement.querySelector('.card__delete-button').addEventListener('click', function() {
+    removeCardFunction(newCardElement)
+  })
+  return newCardElement
+}
+
+function removeCard(cardTag) {
+    cardTag.remove()
+  }
+
+initialCards.forEach(cardInfo => {
+  const newCard = createCard(cardInfo, removeCard)
+  cardsList.append(newCard)
+})
