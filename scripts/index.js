c0e526fe8f36b1b262570aa0b514185e7b11d730
dantum <dantum150@yandex.ru>
2024-02-15 10:21:54 +0300

    Рефакторинг от 15.02.2024

--- scripts/index.js
+++ scripts/index.js
@@ -7,16 +7,20 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
 const cardsList = document.querySelector('.places__list')
 
 function createCard(cardInfo, removeCardFunction) {
-  const newCardTemplate = document.querySelector('#card-template').cloneNode(true).content
+  const newCardTemplate = document.querySelector('#card-template').content
 
-  const newCardElement = newCardTemplate.querySelector('.places__item')
+  const newCardElement = newCardTemplate.querySelector('.places__item').cloneNode(true)
 
   newCardElement.querySelector('.card__title').textContent = cardInfo.name
 
-  newCardElement.querySelector('.card__image').setAttribute('src', cardInfo.link)
+  newCardElement.querySelector('.card__image').src = cardInfo.link
+
+  newCardElement.querySelector('.card__image').alt = cardInfo.name
 
   newCardElement.querySelector('.card__delete-button').addEventListener('click', function() {
     removeCardFunction(newCardElement)
